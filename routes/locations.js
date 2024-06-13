79ec34adc3bf8c3940100b342a92625c4c13b846
ALFAROSO <alberth.sotelo@gmail.com>
2024-06-12 17:30:54 -0700

    create route for Location GET

--- routes/locations.js
+++ routes/locations.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const {
     createLocation,
+    getLocations,
 } = require('../controllers/locationController');
 
 const router = express.Router();
 
 //POST: /api/location
 router.post("/", createLocation)
+
+//GET: /api/location
+router.get("/", getLocations)
 
 module.exports = router;
