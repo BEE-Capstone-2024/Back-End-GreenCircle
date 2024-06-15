9ca7d6027e527ebff59e19d7a219a6267ba4ece7
ALFAROSO <alberth.sotelo@gmail.com>
2024-06-14 19:44:39 -0700

    Update user model to verify email

--- models/User.js
+++ models/User.js
@@ -11,6 +11,7 @@ const userSchema = new mongoose.Schema(
     email: {
       type: String,
       required: [true, "Email is required"],
+      match: [/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please fill a valid email address'],
       unique: true,
     },
     password: {
