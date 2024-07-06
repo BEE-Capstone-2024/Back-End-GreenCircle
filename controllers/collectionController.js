72aec66bf4205929a2a7b37725df7b3b5a7ab906
ALFAROSO <alberth.sotelo@gmail.com>
2024-07-06 11:22:17 -0700

    add more info on getCollections

--- controllers/collectionController.js
+++ controllers/collectionController.js
@@ -7,6 +7,7 @@ const getCollections = async (req, res) => {
 
   if (typeof collectionId == "undefined") {
     const userId = getUserIdByToken(req.headers);
+    console.log(userId);
     if (!userId) {
       res.status(403).json({
         success: false,
@@ -14,18 +15,58 @@ const getCollections = async (req, res) => {
       });
       return;
     }
-    await Collection.find({})
-      .populate("user")
-      .populate("event")
-      .exec()
-      .then((results) => {
-        res
-          .status(results == null ? 404 : 200)
-          .json(results.toJSON({ virtuals: true }));
-      })
-      .catch((err) => {
-        res.status(500).json(err);
+    await Collection.find({ user: userId })
+    .populate("event")
+    .exec()
+    .then((results) => {
+      if (!results || results.length === 0) {
+        res.status(404).json({
+          success: false,
+          message: "No collections found for this user.",
+        });
+      } else {
+        results.sort((a, b) => new Date(a.event.dateOfEvent) - new Date(b.event.dateOfEvent));
+  
+        let materialSums = {};
+        let totalMaterialCount = 0;
+        let totalEventCount = results.length;
+        let eventDatesWithSequence = [];
+        let eventCounter = 0;
+  
+        const collections = results.map(result => {
+          const collection = result.toJSON({ virtuals: true });
+          
+          eventCounter += 1;
+          eventDatesWithSequence.push({ dateOfEvent: collection.event.dateOfEvent, sequence: eventCounter });
+  
+          collection.counts.forEach(count => {
+            if (!materialSums[count.material]) {
+              materialSums[count.material] = 0;
+            }
+            materialSums[count.material] += count.count;
+            totalMaterialCount += count.count;
+          });
+  
+          return collection;
+        });
+  
+        res.status(200).json({
+          success: true,
+          collections: collections,
+          materialSums: materialSums,
+          totalMaterialCount: totalMaterialCount,
+          totalEventCount: totalEventCount,
+          eventDatesWithSequence: eventDatesWithSequence,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server error",
+        error: err.message,
       });
+    });
   } else {
     await Collection.findOne({ _id: collectionId })
       .populate("user")
